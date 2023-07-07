bef638b363a1231aee0dc1d9023ae23fd97e4c7c
elpaablo <donopwhile1@gmail.com>
2023-07-07 13:51:27 +0200

    Interfaces: Battery styles: add some extra validation
    Signed-off-by: Iverz <asterixiverz@gmail.com>
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- src/com/awaken/support/fragments/battery/StatusBarBatterySettings.java
+++ src/com/awaken/support/fragments/battery/StatusBarBatterySettings.java
@@ -18,118 +18,152 @@ package com.awaken.support.fragments.battery;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 
-import androidx.preference.PreferenceCategory;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
-import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
 
+import com.android.internal.logging.nano.MetricsProto;
+
 import com.android.settings.R;
-import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
+import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
+
+import com.awaken.support.preferences.SystemSettingListPreference;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SearchIndexable
 public class StatusBarBatterySettings extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceChangeListener, Indexable {
+            implements Preference.OnPreferenceChangeListener  {
 
-    private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
-    private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
-
-    private ListPreference mBatteryPercent;
-    private ListPreference mBatteryStyle;
-
-    private int mBatteryPercentValue;
+    private static final String KEY_STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+    private static final String KEY_STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    private static final String KEY_STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
 
     private static final int BATTERY_STYLE_PORTRAIT = 0;
     private static final int BATTERY_STYLE_TEXT = 4;
     private static final int BATTERY_STYLE_HIDDEN = 5;
+    private static final int BATTERY_STYLE_IOS16 = 18;
+
     private static final int BATTERY_PERCENT_HIDDEN = 0;
-    //private static final int BATTERY_PERCENT_SHOW_INSIDE = 1;
-    //private static final int BATTERY_PERCENT_SHOW_OUTSIDE = 2;
+    private static final int BATTERY_PERCENT_INSIDE = 1;
+    private static final int BATTERY_PERCENT_RIGHT = 2;
+    private static final int BATTERY_PERCENT_LEFT = 3;
+
+    private SwitchPreference mBatteryTextCharging;
+    private SystemSettingListPreference mBatteryPercent;
+    private SystemSettingListPreference mBatteryStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.statusbar_battery_settings);
+
+        addPreferencesFromResource(R.xml.battery_settings);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
 
         int batterystyle = Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
-        mBatteryStyle = (ListPreference) findPreference("status_bar_battery_style");
-        mBatteryStyle.setValue(String.valueOf(batterystyle));
-        mBatteryStyle.setSummary(mBatteryStyle.getEntry());
+        int batterypercent = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
+
+        mBatteryStyle = (SystemSettingListPreference) findPreference(KEY_STATUS_BAR_BATTERY_STYLE);
         mBatteryStyle.setOnPreferenceChangeListener(this);
 
-        mBatteryPercentValue = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, BATTERY_PERCENT_HIDDEN, UserHandle.USER_CURRENT);
-        mBatteryPercent = (ListPreference) findPreference("status_bar_show_battery_percent");
-        mBatteryPercent.setValue(String.valueOf(mBatteryPercentValue));
-        mBatteryPercent.setSummary(mBatteryPercent.getEntry());
+        mBatteryPercent = (SystemSettingListPreference) findPreference(KEY_STATUS_BAR_SHOW_BATTERY_PERCENT);
         mBatteryPercent.setOnPreferenceChangeListener(this);
-        mBatteryPercent.setEnabled(
-                batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN);
 
+        handleBatteryPercent(batterystyle, batterypercent);
+
+        mBatteryTextCharging = (SwitchPreference) findPreference(KEY_STATUS_BAR_BATTERY_TEXT_CHARGING);
+        mBatteryTextCharging.setEnabled(batterystyle != BATTERY_STYLE_TEXT &&
+                (batterypercent == BATTERY_PERCENT_INSIDE || batterypercent == BATTERY_PERCENT_HIDDEN));
+    }
+
+    private void handleBatteryPercent(int batterystyle, int batterypercent) {
+        if (batterystyle < BATTERY_STYLE_TEXT) {
+            mBatteryPercent.setEntries(R.array.status_bar_battery_percent_entries);
+            mBatteryPercent.setEntryValues(R.array.status_bar_battery_percent_values);;
+        }
+        else {
+            mBatteryPercent.setEntries(R.array.status_bar_battery_percent_no_text_inside_entries);
+            mBatteryPercent.setEntryValues(R.array.status_bar_battery_percent_no_text_inside_values);
+            if (batterystyle == BATTERY_STYLE_IOS16) {
+                if (batterypercent != BATTERY_PERCENT_INSIDE) {
+                    batterypercent = BATTERY_PERCENT_INSIDE;
+                    mBatteryPercent.setValueIndex(BATTERY_PERCENT_INSIDE);
+                }
+            } else if (batterypercent == BATTERY_PERCENT_INSIDE) {
+                batterypercent = BATTERY_PERCENT_HIDDEN;
+                mBatteryPercent.setValueIndex(BATTERY_PERCENT_HIDDEN);
+            }
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT,
+                    batterypercent, UserHandle.USER_CURRENT);
+        }
+
+        mBatteryPercent.setEnabled(
+                batterystyle != BATTERY_STYLE_TEXT &&
+                batterystyle != BATTERY_STYLE_HIDDEN &&
+                batterystyle != BATTERY_STYLE_IOS16);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mBatteryStyle) {
             int batterystyle = Integer.parseInt((String) newValue);
-            Settings.System.putIntForUser(resolver,
-                Settings.System.STATUS_BAR_BATTERY_STYLE, batterystyle,
-                UserHandle.USER_CURRENT);
-            int index = mBatteryStyle.findIndexOfValue((String) newValue);
-            mBatteryStyle.setSummary(mBatteryStyle.getEntries()[index]);
-            mBatteryPercent.setEnabled(
-                    batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN);
+            int batterypercent = Settings.System.getIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
+            handleBatteryPercent(batterystyle, batterypercent);
+            mBatteryTextCharging.setEnabled(batterystyle != BATTERY_STYLE_TEXT &&
+                    (batterypercent == BATTERY_PERCENT_INSIDE || batterypercent == BATTERY_PERCENT_HIDDEN));
             return true;
         } else if (preference == mBatteryPercent) {
-            mBatteryPercentValue = Integer.parseInt((String) newValue);
-            Settings.System.putIntForUser(resolver,
-                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, mBatteryPercentValue,
-                    UserHandle.USER_CURRENT);
-            int index = mBatteryPercent.findIndexOfValue((String) newValue);
-            mBatteryPercent.setSummary(mBatteryPercent.getEntries()[index]);
+            int batterypercent = Integer.parseInt((String) newValue);
+            int batterystyle = Settings.System.getIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
+            mBatteryTextCharging.setEnabled(batterystyle != BATTERY_STYLE_TEXT &&
+                    (batterypercent == BATTERY_PERCENT_INSIDE || batterypercent == BATTERY_PERCENT_HIDDEN));
             return true;
         }
         return false;
-    }
-
+    }  
+    
     @Override
     public int getMetricsCategory() {
-        return -1;
+        return MetricsProto.MetricsEvent.AWAKEN;
     }
 
     public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider() {
-                @Override
-                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                                                                            boolean enabled) {
-                    ArrayList<SearchIndexableResource> result =
-                            new ArrayList<SearchIndexableResource>();
+        new BaseSearchIndexProvider() {
+            @Override
+            public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                    boolean enabled) {
+                final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                final SearchIndexableResource sir = new SearchIndexableResource(context);
+                sir.xmlResId = R.xml.battery_settings;
+                result.add(sir);
+                return result;
+            }
 
-                    SearchIndexableResource sir = new SearchIndexableResource(context);
-                    sir.xmlResId = R.xml.statusbar_battery_settings;
-                    result.add(sir);
-                    return result;
-                }
-
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    List<String> keys = super.getNonIndexableKeys(context);
-                    return keys;
-                }
-            };
+            @Override
+            public List<String> getNonIndexableKeys(Context context) {
+                final List<String> keys = super.getNonIndexableKeys(context);
+                return keys;
+            }
+    };
 }
