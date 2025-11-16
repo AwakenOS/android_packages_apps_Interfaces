921b9c6cb6487cdad9d0f9814104ec90a1f0c6eb
OliverSyx <173170056+OliverSyx@users.noreply.github.com>
2025-11-16 23:47:54 +0700

    Interfaces: Add option to launch display customizations
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- src/com/awaken/support/fragments/StatusBar.java
+++ src/com/awaken/support/fragments/StatusBar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 SuperiorOS
+ * Copyright (C) 2021 Wave-OS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,62 +16,155 @@
 
 package com.awaken.support.fragments;
 
-import com.android.internal.logging.nano.MetricsProto;
-
-import android.os.Bundle;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.os.UserHandle;
+import android.app.Activity;
+import android.content.Context;
 import android.content.ContentResolver;
 import android.content.res.Resources;
-import androidx.preference.ListPreference;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceGroup;
-import androidx.preference.PreferenceScreen;
-import androidx.preference.PreferenceCategory;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.Settings;
-import com.android.settings.R;
-
-import java.util.Locale;
-import android.text.TextUtils;
+import android.text.format.DateFormat;
+import android.view.Display;
+import android.view.DisplayCutout;
+import android.view.Surface;
 import android.view.View;
 
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
-import android.util.Log;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Collections;
+import com.awaken.support.preferences.SecureSettingMasterSwitchPreference;
+import com.awaken.support.preferences.SecureSettingListPreference;
+import com.awaken.support.preferences.SystemSettingMasterSwitchPreference;
 
-public class StatusBar extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+public class StatusBar extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "Statusbar category";
+    private static final String BRIGHTNESS_SLIDER = "qs_show_brightness";
+    private static final String KEY_BATTERY_CHARGING_LIGHT = "battery_charging_light";
+    private static final String KEY_CLOCK_POSITION = "status_bar_clock_position";
+    private static final String KEY_CLOCK_AM_PM = "status_bar_am_pm";
+    private static final String KEY_NETWORK_TRAFFIC = "network_traffic_state";
+
+    private SecureSettingMasterSwitchPreference mBrightnessSlider;
+    private SecureSettingListPreference mClockPositionPref;
+    private SecureSettingListPreference mAmPmPref;
+    private SystemSettingMasterSwitchPreference mNetworkTraffic;
+    Preference mBatteryLightPref;
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-
-        addPreferencesFromResource(R.xml.superior_lab_statusbar);
-
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.awaken_statusbar);
         PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
 
+        mBrightnessSlider = (SecureSettingMasterSwitchPreference)
+                findPreference(BRIGHTNESS_SLIDER);
+        mBrightnessSlider.setOnPreferenceChangeListener(this);
+        boolean enabled = Settings.Secure.getInt(resolver,
+                BRIGHTNESS_SLIDER, 1) == 1;
+        mBrightnessSlider.setChecked(enabled);
+
+        mBatteryLightPref = (Preference) findPreference(KEY_BATTERY_CHARGING_LIGHT);
+        if (!getResources()
+                .getBoolean(com.android.internal.R.bool.config_intrusiveBatteryLed))
+        {
+			if (mBatteryLightPref != null) {
+				prefSet.removePreference(mBatteryLightPref);
+			}
+        }
+
+        mClockPositionPref = (SecureSettingListPreference) findPreference(KEY_CLOCK_POSITION);
+        mAmPmPref = (SecureSettingListPreference) findPreference(KEY_CLOCK_AM_PM);
+
+        boolean hasNotch = hasCenteredCutout(getActivity());
+        boolean isRtl = getResources().getConfiguration().getLayoutDirection()
+                == View.LAYOUT_DIRECTION_RTL;
+
+        // Adjust clock position pref for RTL and center notch
+        int entries = hasNotch ? R.array.status_bar_clock_position_entries_notch
+                : R.array.status_bar_clock_position_entries;
+        int values = hasNotch ? (isRtl ? R.array.status_bar_clock_position_values_notch_rtl
+                : R.array.status_bar_clock_position_values_notch)
+                : (isRtl ? R.array.status_bar_clock_position_values_rtl
+                : R.array.status_bar_clock_position_values);
+        mClockPositionPref.setEntries(entries);
+        mClockPositionPref.setEntryValues(values);
+
+        // Disable AM/PM for 24-hour format
+        if (DateFormat.is24HourFormat(getActivity())) {
+            mAmPmPref.setEnabled(false);
+            mAmPmPref.setSummary(R.string.status_bar_am_pm_disabled);
+        }
+
+        mNetworkTraffic = (SystemSettingMasterSwitchPreference)
+                findPreference(KEY_NETWORK_TRAFFIC);
+        enabled = Settings.System.getIntForUser(resolver,
+                KEY_NETWORK_TRAFFIC, 0, UserHandle.USER_CURRENT) == 1;
+        mNetworkTraffic.setChecked(enabled);
+        mNetworkTraffic.setOnPreferenceChangeListener(this);
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mBrightnessSlider) {
+            boolean value = (boolean) newValue;
+            Settings.Secure.putInt(resolver,
+                    BRIGHTNESS_SLIDER, value ? 1 : 0);
+            return true;
+        } else if (preference == mNetworkTraffic) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putIntForUser(resolver, KEY_NETWORK_TRAFFIC,
+                    value ? 1 : 0, UserHandle.USER_CURRENT);
+            return true;
+        }
         return false;
     }
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.AWAKEN;
+       return MetricsProto.MetricsEvent.AWAKEN;
     }
 
+    /* returns whether the device has a centered display cutout or not. */
+    private static boolean hasCenteredCutout(Context context) {
+        Display display = context.getDisplay();
+        DisplayCutout cutout = display.getCutout();
+        if (cutout != null) {
+            Point realSize = new Point();
+            display.getRealSize(realSize);
+
+            switch (display.getRotation()) {
+                case Surface.ROTATION_0: {
+                    Rect rect = cutout.getBoundingRectTop();
+                    return !(rect.left <= 0 || rect.right >= realSize.x);
+                }
+                case Surface.ROTATION_90: {
+                    Rect rect = cutout.getBoundingRectLeft();
+                    return !(rect.top <= 0 || rect.bottom >= realSize.y);
+                }
+                case Surface.ROTATION_180: {
+                    Rect rect = cutout.getBoundingRectBottom();
+                    return !(rect.left <= 0 || rect.right >= realSize.x);
+                }
+                case Surface.ROTATION_270: {
+                    Rect rect = cutout.getBoundingRectRight();
+                    return !(rect.top <= 0 || rect.bottom >= realSize.y);
+                }
+            }
+        }
+        return false;
+    }
 }
